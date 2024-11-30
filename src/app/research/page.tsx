4cbee73f4b86ed54d97471800612cd4eda90f2fa
Timothy Medewase <medewaset@gmail.com>
2024-11-29 18:16:31 -0600

    Update dependencies and enhance page animations

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,13 +1,29 @@
+"use client";
 import React from "react";
-import Footer from "@/components/Footer";
+import Footer from "../../components/Footer";
+import { motion } from "framer-motion";
 
 const Research = () => {
   return (
     <div>
-      <main className="mt-5 md:mx-auto lg:mx-12">
-        <div className="flex max-w-fit mt-5 text-xl">Coming Soon...</div>
-        <Footer />
-      </main>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <main className="mt-5 md:mx-auto lg:mx-12">
+          <div className="mt-5 mb-12 font-sans font-bold text-5xl max-w-fit mx-auto">
+            Research
+          </div>
+          <div className="flex max-w-fit mt-5 text-xl">Coming Soon...</div>
+          <Footer />
+        </main>
+      </motion.div>
     </div>
   );
 };
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,21 +1,34 @@
-import React from "react";
+"use client";
 import Footer from "@/components/Footer";
+import { motion } from "framer-motion";
 
-const Resume = () => {
+export default function Resume() {
   return (
     <div>
-      <main className="mt-5 md:mx-auto lg:mx-12">
-        <div className="flex max-w-fit mx-auto mt-5 ">
-          <iframe
-            src="TimothyMedewase_2025Resume.pdf"
-            width="1200 px"
-            height="900 px"
-          />
-        </div>
-        <Footer />
-      </main>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <main className="mt-5 md:mx-auto lg:mx-12">
+          <div className="mt-5 mb-12 font-sans font-bold text-5xl max-w-fit mx-auto">
+            Resume
+          </div>
+          <div className="flex max-w-fit mx-auto mt-5 ">
+            <iframe
+              src="/TimothyMedewase_2025Resume.pdf"
+              width="1200"
+              height="900"
+            />
+          </div>
+          <Footer />
+        </main>
+      </motion.div>
     </div>
   );
-};
-
-export default Resume;
+}
