062003b8f987c6ce8d88e2b8bf9bb58ecd13f92f
Timothy Medewase <medewaset@gmail.com>
2025-03-24 17:27:48 -0500

    Update FloatingNav component: change icon prop type from JSX.Element to ReactElement

--- src/components/ui/FloatingNav.tsx
+++ src/components/ui/FloatingNav.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { ReactElement } from "react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -13,7 +13,7 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
-    icon?: JSX.Element;
+    icon?: ReactElement;
   }[];
   className?: string;
 }) => {
