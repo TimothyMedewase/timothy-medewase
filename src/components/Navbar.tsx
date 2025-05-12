f69027672e1274685d604c3a8bb10494c409b873
Timothy Medewase <medewaset@gmail.com>
2025-05-12 01:39:37 -0400

    Comment out the "Research" nav item in the Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,11 +21,11 @@ export function Navbar() {
       link: "/about",
       icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
-    {
-      name: "Research",
-      link: "/research",
-      icon: <IconBook className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
+    // {
+    //   name: "Research",
+    //   link: "/research",
+    //   icon: <IconBook className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    // },
     {
       name: "Projects",
       link: "/projects",
